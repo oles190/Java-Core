fb700269a41d751b797c387709d2b1af21ef22e9
Oles <doskucho@gmail.com>
2021-10-13 12:48:29 +0300

    lesson1

---
+++ Lesson1/Hello.java
@@ -0,0 +1,15 @@
+package Lesson1;
+
+public class Hello {
+
+  static int a = 11;
+  static int b=23;
+
+    public static void main(String[] args) {
+
+
+        System.out.println(a+b);
+    }
+
+
+}
